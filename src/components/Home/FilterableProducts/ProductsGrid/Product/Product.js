3046085dfb9bd713104dccdf95e94ce32883147f
Md. Sayeed Rahman <mdsayeedrahman1999@gmail.com>
2022-06-01 16:41:12 +0600

    navbar updated with add to cart button on product component

--- src/components/Home/FilterableProducts/ProductsGrid/Product/Product.js
+++ src/components/Home/FilterableProducts/ProductsGrid/Product/Product.js
@@ -1,24 +1,22 @@
 import React from "react";
+import "./Product.css";
 
 const Product = (props) => {
   const { product } = props;
 
   return (
     <div style={{ margin: "10px 0" }}>
-      <img src={product.img} className="img-thumbnail" />
-      <p
-        style={{
-          backgroundColor: "#efefef",
-          margin: "2px",
-          padding: "0 2px",
-        }}
-      >
-        {product.name}
-      </p>
+      <img
+        src={product.img}
+        className="img-thumbnail"
+        style={{ height: "250px", width: "310px" }}
+      />
+      <p className="productName">{product.name}</p>
       <div className="d-flex justify-content-between">
         <div> {product.price} </div>
         <div> {product.stocked ? "In Stock" : "Out Of Stock"} </div>
       </div>
+      <p className="addToCartButton">Add To Cart</p>
     </div>
   );
 };
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,8 +4,17 @@ import "./NavBar.css";
 const NavBar = (props) => {
   return (
     <>
-      <nav className="navbar bg-light px-2">
+      <nav className="navbar bg-light d-flex justify-content-start">
         <span className="app-title"> Problem Solver's Caffe! </span>
+        <span style={{ color: "royalblue", fontWeight: "bold" }}>
+          Chit-Chat!
+        </span>
+        <span style={{ color: "green", fontWeight: "bold" }}>Order Food!</span>
+        <span
+          style={{ marginLeft: "auto", fontStyle: "italic", color: "magenta" }}
+        >
+          <span style={{ fontWeight: "bold" }}>Welcome</span>, guest!
+        </span>
       </nav>
       <div className="spacerBelowNavbar"></div>
     </>
