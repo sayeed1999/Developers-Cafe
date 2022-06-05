af5135154897226fcb9f383221767cd0c5fc459a
Md. Sayeed Rahman <mdsayeedrahman1999@gmail.com>
2022-06-05 11:43:11 +0600

    added axios, setup axios http interceptor, added Loader component, but could not show loader from interceptor :(

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
+import axios from "axios";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
+
+axios.interceptors.request.use(
+  (req) => {
+    // Do what you want to do here..
+    return req;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
+
+axios.interceptors.response.use(
+  (res) => {
+    // Add configurations here..
+    if (res.status === 201) {
+      console.log("Posted successfully");
+    }
+    console.log(res);
+    return res;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
