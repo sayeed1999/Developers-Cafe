a7753f0d4ee814a5ed82cb118e665a792cfdaa0f
Md. Sayeed Rahman <mdsayeedrahman1999@gmail.com>
2022-07-18 00:18:33 +0600

    chatroom made responsive with sliding roomlist from top in small screen

--- pages/chatroom/index.js
+++ pages/chatroom/index.js
@@ -1,5 +1,7 @@
+import MenuIcon from "@mui/icons-material/Menu";
+import { IconButton } from "@mui/material";
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { createRef, useState } from "react";
 import Room from "../../components/modules/chatroom/Room";
 import AppRoutes from "../../constants/AppRoutes";
 import styles from "../../styles/Chatroom.module.scss";
@@ -8,20 +10,44 @@ const Chatroom = () => {
   const router = useRouter();
   const rooms = ["help", "general", "fun"];
   const [room, setRoom] = useState(rooms[0]);
+  const roomlistRef = createRef();
 
-  const roomlist = rooms.map((r, index) => (
-    <div
-      key={index}
-      className={styles.roomlistitem}
-      onClick={() => router.push(`${AppRoutes.Chatroom}#${r}`)}
-    >
-      #{r}
-    </div>
-  ));
+  const toggleMenu = () => {
+    // toggle menu position from -150 to 0 and back
+    if (roomlistRef.current.style.top !== "-150px") {
+      roomlistRef.current.style.top = "-150px";
+    } else {
+      roomlistRef.current.style.top = "50px";
+    }
+  };
+
+  const roomlist = (
+    <>
+      {rooms.map((r, index) => (
+        <div
+          key={index}
+          className={styles.roomlistitem}
+          onClick={() => {
+            router.push(`${AppRoutes.Chatroom}#${r}`);
+            toggleMenu();
+          }}
+        >
+          #{r}
+        </div>
+      ))}
+      <div className="text-center d-md-none">
+        <IconButton color="inherit" onClick={toggleMenu}>
+          <MenuIcon />
+        </IconButton>
+      </div>
+    </>
+  );
 
   return (
-    <div className="row">
-      <div className={`${styles.roomlist} col-md-3`}>{roomlist}</div>
+    <div className={`${styles.chatroom} row`}>
+      <div ref={roomlistRef} className={`${styles.roomlist} col-md-3`}>
+        {roomlist}
+      </div>
       <div className={`${styles.room} col-md-9`}>
         <Room />
       </div>
